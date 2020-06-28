6a987aed56d7fd350dbfd39ba731a4d56f651158
Sasu <git@sasu.net>
2020-06-28 11:06:44 +0200

    First basically working version

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"math/rand"
 	"net/http"
 	"strconv"
@@ -16,26 +18,44 @@ func main() {
 	m = make(map[uint64]string)
 
 	r := mux.NewRouter()
+	r.NotFoundHandler = http.HandlerFunc(NotFoundHandler)
 
 	r.HandleFunc("/string/{string}", func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
 		string := vars["string"]
+		fmt.Println("Received request POST /string/" + string)
 
 		pin := uint64(rand.Intn(10000))
 		m[pin] = string
 
-		fmt.Fprintf(w, "You have posted string: %s\n", string)
-
-		sPin := strconv.FormatUint(pin, 32)
-		w.Write([]byte(sPin))
+		sPin := strconv.FormatUint(pin, 10)
+		fmt.Println("Returning created PIN " + sPin)
+		io.WriteString(w, sPin)
 	}).Methods("POST")
 
 	r.HandleFunc("/pin/{pin}", func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
-		pin := vars["pin"]
+		pinStr := vars["pin"]
+		fmt.Println("Received request GET /pin/" + pinStr)
 
-		fmt.Fprintf(w, "You have posted PIN: %s\n", pin)
+		pin, err := strconv.ParseUint(pinStr, 10, 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pwd := m[pin]
+		if pwd == "" {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		fmt.Println("Returning retrieved string " + pwd)
+		io.WriteString(w, pwd)
 	}).Methods("GET")
 
 	http.ListenAndServe(":10000", r)
+}
+
+func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintf(w, "404 - Not Found")
 }
