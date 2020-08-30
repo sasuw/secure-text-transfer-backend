17bb357f7e5202a810986947673e51e791d23b3d
Sasu <git@sasu.net>
2020-08-30 18:55:27 +0200

    Added REST API GET and POST methods /encryptedText in preparation of storing client-sidedly encrypted text values

---
+++ cmd/jsonHandler.go
@@ -0,0 +1,98 @@
+/*
+Copyright Alex Edwards
+
+Adapted from code at https://www.alexedwards.net/blog/how-to-properly-parse-a-json-request-body
+
+Licensed under MIT License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type malformedRequest struct {
+	status int
+	msg    string
+}
+
+func (mr *malformedRequest) Error() string {
+	return mr.msg
+}
+
+func decodeJSONBody(w http.ResponseWriter, r *http.Request, dst interface{}) error {
+	cthv := r.Header.Get("Content-Type")
+	if cthv != "" {
+		if cthv != "application/json" {
+			msg := "Content-Type header is not application/json"
+			return &malformedRequest{status: http.StatusUnsupportedMediaType, msg: msg}
+		}
+	}
+
+	r.Body = http.MaxBytesReader(w, r.Body, 1048576)
+
+	dec := json.NewDecoder(r.Body)
+	//dec.DisallowUnknownFields()
+
+	err := dec.Decode(&dst)
+	if err != nil {
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+
+		switch {
+		case errors.As(err, &syntaxError):
+			msg := fmt.Sprintf("Request body contains badly-formed JSON (at position %d)", syntaxError.Offset)
+			return &malformedRequest{status: http.StatusBadRequest, msg: msg}
+
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			msg := fmt.Sprintf("Request body contains badly-formed JSON")
+			return &malformedRequest{status: http.StatusBadRequest, msg: msg}
+
+		case errors.As(err, &unmarshalTypeError):
+			msg := fmt.Sprintf("Request body contains an invalid value for the %q field (at position %d)", unmarshalTypeError.Field, unmarshalTypeError.Offset)
+			return &malformedRequest{status: http.StatusBadRequest, msg: msg}
+
+		/*
+			case strings.HasPrefix(err.Error(), "json: unknown field "):
+				fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
+				msg := fmt.Sprintf("Request body contains unknown field %s", fieldName)
+				return &malformedRequest{status: http.StatusBadRequest, msg: msg}
+		*/
+
+		case errors.Is(err, io.EOF):
+			msg := "Request body must not be empty"
+			return &malformedRequest{status: http.StatusBadRequest, msg: msg}
+
+		case err.Error() == "http: request body too large":
+			msg := "Request body must not be larger than 1MB"
+			return &malformedRequest{status: http.StatusRequestEntityTooLarge, msg: msg}
+
+		default:
+			return err
+		}
+	}
+
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		msg := "Request body must only contain a single JSON object"
+		return &malformedRequest{status: http.StatusBadRequest, msg: msg}
+	}
+
+	return nil
+}
--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	crand "crypto/rand"
 	"encoding/binary"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -16,14 +18,103 @@ import (
 	"github.com/gorilla/mux"
 )
 
+/*
+Payload represents data sent from client when storing client-sidedly encrypted text
+*/
+type Payload struct {
+	Ciphertext string
+	Iv         string
+	Salt       string
+	Id         string
+}
+
 var m map[string]string
+var mp map[string]Payload
 
 func main() {
 
 	m = make(map[string]string)
+	mp = make(map[string]Payload)
 
 	r := mux.NewRouter()
 	r.NotFoundHandler = http.HandlerFunc(NotFoundHandler)
+
+	r.HandleFunc("/encryptedText", func(w http.ResponseWriter, r *http.Request) {
+		initResult := InitRequest(w, r)
+		if !initResult {
+			return
+		}
+
+		dt := time.Now()
+		fmt.Println("Received request to store json at ", dt.String())
+
+		var p Payload
+
+		err := decodeJSONBody(w, r, &p)
+		if err != nil {
+			var mr *malformedRequest
+			if errors.As(err, &mr) {
+				http.Error(w, mr.msg, mr.status)
+			} else {
+				log.Println(err.Error())
+				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			}
+			return
+		}
+
+		if p.Ciphertext == "" || p.Id == "" || p.Iv == "" || p.Salt == "" {
+			log.Println("Required payload field missing")
+			w.WriteHeader(http.StatusBadRequest) //http 400
+			return
+		}
+
+		fmt.Fprintf(w, "Payload: %+v", p)
+		mp[p.Id] = p
+
+	}).Methods("POST", "OPTIONS")
+
+	r.HandleFunc("/encryptedText", func(w http.ResponseWriter, r *http.Request) {
+		initResult := InitRequest(w, r)
+		if !initResult {
+			return
+		}
+
+		id, _ := r.URL.Query()["id"]
+
+		if len(id) > 1 {
+			log.Fatal("Multiple IDs not allowed")
+			w.WriteHeader(http.StatusBadRequest) //http 400
+			return
+		}
+		idStr := id[0]
+
+		dt := time.Now()
+		blen := strconv.Itoa(len(idStr))
+		fmt.Println("Received request to retrieve encrypted text with passphrase of length "+blen+" at ", dt.String())
+		//TODO: make environment-specific logging for debugging purposes
+		//fmt.Println("Received request POST /pin with body " + pinStr)
+
+		p := mp[idStr]
+		if p == (Payload{}) {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		delete(mp, idStr)
+
+		pJson, err := json.Marshal(p)
+		if err != nil {
+			log.Fatal(err)
+			w.WriteHeader(http.StatusInternalServerError) //http 500
+			return
+		}
+
+		//TODO: make environment-specific logging for debugging purposes
+		pJsonStr := string(pJson)
+		fmt.Println("Returning retrieved struct " + pJsonStr)
+
+		io.WriteString(w, pJsonStr)
+	}).Methods("GET")
 
 	r.HandleFunc("/string", func(w http.ResponseWriter, r *http.Request) {
 		initResult := InitRequest(w, r)
@@ -53,7 +144,7 @@ func main() {
 		maxTextLength := 4000
 		if len(string) > maxTextLength {
 			//4000 characters should be enough for everybody
-			http.Error(w, "Text too long ("+strconv.Itoa(maxTextLength)+" characters max)", http.StatusBadRequest)
+			http.Error(w, "Text too long ("+strconv.Itoa(maxTextLength)+" characters max)", http.StatusBadRequest) //400
 			return
 		}
 
@@ -143,8 +234,8 @@ func (s cryptoSource) Uint64() (v uint64) {
 }
 
 /*
-GetPin returns a PIN of type uint64 with a value between 1 and 99999
-or 0 if no PIN could be generated due to collisions
+GetPin returns a PIN of type string with a value between "1" and "99999"
+or "0" if no PIN could be generated due to collisions
 */
 func GetPin() string {
 	var src cryptoSource
@@ -190,7 +281,7 @@ func InitRequest(w http.ResponseWriter, r *http.Request) bool {
 	xReqW := r.Header.Get("X-Requested-With")
 
 	if xReqW != "XMLHttpRequest" {
-		http.Error(w, "Bad header", http.StatusBadRequest)
+		http.Error(w, "Bad header", http.StatusBadRequest) //400
 		return false
 	}
 
